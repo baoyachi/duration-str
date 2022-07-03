0e076a5b73b533bb03636e5dc77af4ace12ba031
baoyachi <liaoymxsdl@gmail.com>
2022-07-03 16:46:52 +0800

    fix #8

---
+++ examples/deserialize_duration_time.rs
@@ -0,0 +1,15 @@
+use time::Duration;
+use duration_str::deserialize_duration_time;
+use serde::*;
+
+#[derive(Debug, Deserialize)]
+struct Config {
+    #[serde(deserialize_with = "deserialize_duration_time")]
+    time_ticker: Duration,
+}
+
+fn main() {
+    let json = r#"{"time_ticker":"1m+30"}"#;
+    let config: Config = serde_json::from_str(json).unwrap();
+    assert_eq!(config.time_ticker, Duration::seconds(60 + 30));
+}
