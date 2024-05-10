7aa131e85fce0c23a74d184ff88ead56ea709a8a
baoyachi <liaoymxsdl@gmail.com>
2024-05-10 23:49:31 +0800

    fix parsing error location precise prompt

--- src/parser.rs
+++ src/parser.rs
@@ -1,84 +1,47 @@
-use crate::{Calc, CondUnit, TimeUnit};
-use std::fmt::{Display, Formatter};
+use crate::error::PError;
+use crate::unit::opt_unit_abbr;
+use crate::{Calc, CondUnit, ExpectErr, TimeUnit};
 use std::time::Duration;
 use winnow::ascii::{digit1, multispace0};
-use winnow::combinator::{alt, eof, opt, repeat};
-use winnow::error::{ErrMode, ErrorKind, FromExternalError, ParserError};
-use winnow::stream::{AsChar, Stream};
-use winnow::token::take_while;
+use winnow::combinator::{alt, eof, opt, peek, repeat};
+use winnow::error::ErrMode;
 use winnow::PResult;
 use winnow::Parser;
 
-#[derive(Debug, PartialEq, Eq)]
-pub struct PError<I> {
-    partial_input: I,
-    kind: ErrorKind,
-    cause: String,
-}
-
-impl<I> PError<I> {
-    fn new(input: I, kind: ErrorKind) -> Self {
-        PError {
-            partial_input: input,
-            kind,
-            cause: "".to_string(),
-        }
-    }
-}
-
-impl<I: Stream + Clone> ParserError<I> for PError<I> {
-    fn from_error_kind(input: &I, kind: ErrorKind) -> Self {
-        PError::new(input.clone(), kind)
-    }
-
-    fn append(self, _: &I, _: &<I as Stream>::Checkpoint, _: ErrorKind) -> Self {
-        self
-    }
-}
-
-impl<I: Clone, E: std::error::Error + Send + Sync + 'static> FromExternalError<I, E> for PError<I> {
-    #[inline]
-    fn from_external_error(input: &I, kind: ErrorKind, e: E) -> Self {
-        let mut err = Self::new(input.clone(), kind);
-        {
-            err.cause = e.to_string();
-        }
-        err
-    }
-}
-
-impl<I> Display for PError<I>
-where
-    I: Display,
-{
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "partial_input:`{}`,{}", self.partial_input, self.kind)?;
-        if !self.cause.is_empty() {
-            write!(f, ",{}", self.cause)?;
-        }
-        Ok(())
-    }
-}
-
-fn unit_abbr<'a>(input: &mut &'a str) -> PResult<TimeUnit, PError<&'a str>> {
-    let checkpoint = input.checkpoint();
-    let val = take_while(1.., |c: char| c.is_alpha() || c == 'µ').parse_next(input)?;
-    str::parse(val).map_err(|err| {
-        input.reset(&checkpoint);
-        ErrMode::from_external_error(input, ErrorKind::Fail, err)
-    })
-}
-
-fn cond_unit<'a>(input: &mut &'a str) -> PResult<CondUnit, PError<&'a str>> {
+fn cond_unit1<'a>(input: &mut &'a str) -> PResult<CondUnit, PError<&'a str>> {
     alt(('+'.value(CondUnit::Plus), '*'.value(CondUnit::Star))).parse_next(input)
 }
 
+fn opt_cond_unit<'a>(input: &mut &'a str) -> PResult<CondUnit, PError<&'a str>> {
+    let multispace = multispace0::<_, PError<_>>;
+    let result = cond_unit1
+        .parse_next(input)
+        .map_err(|err: ErrMode<PError<_>>| {
+            err.map(|x| {
+                let partial_input = x.partial_input;
+                x.append_cause(CondUnit::expect_err(partial_input))
+            })
+        });
+    if result.is_err() {
+        if (multispace, eof).parse_next(input).is_ok() {
+            // The input result is empty except for spaces. Give `TimeUnit` default value
+            return Ok(CondUnit::Plus);
+        }
+
+        if peek((multispace, digit1, opt_unit_abbr, multispace))
+            .parse_next(input)
+            .is_ok()
+        {
+            return Ok(CondUnit::Plus);
+        } else {
+            return Err(result.err().unwrap().cut());
+        }
+    }
+    result
+}
+
 pub(crate) fn parse_expr_time<'a>(input: &mut &'a str) -> PResult<u64, PError<&'a str>> {
-    (
-        multispace0,
-        digit1,
-        alt(((multispace0, eof).value(TimeUnit::default()), unit_abbr)),
-    )
+    (multispace0, digit1, opt_unit_abbr)
         .map(|x| (x.1, x.2))
         .try_map(|(v, unit)| unit.duration(v))
         .parse_next(input)
@@ -91,12 +54,12 @@ pub(crate) fn cond_time<'a>(
         0..,
         (
             multispace0,
-            opt(cond_unit).map(|x| x.unwrap_or(CondUnit::Plus)),
+            opt_cond_unit,
             multispace0,
             digit1,
             // Add by default.
             // Parse unit, default is seconds.
-            opt(unit_abbr).map(Option::unwrap_or_default),
+            opt_unit_abbr,
             multispace0,
         )
             .map(|x| (x.3, x.1, x.4)),
@@ -128,59 +91,7 @@ pub fn parse(input: impl AsRef<str>) -> Result<Duration, String> {
 #[allow(clippy::identity_op)]
 mod tests {
     use super::*;
-    use crate::{CondUnit, TimeUnit};
-    use winnow::Partial;
-
-    #[test]
-    fn test_time_unit_abbr() {
-        assert_eq!(
-            unit_abbr.parse_peek(&Partial::new("y")),
-            Ok(("", TimeUnit::Year))
-        );
-        assert_eq!(
-            unit_abbr.parse_peek(&Partial::new("mon")),
-            Ok(("", TimeUnit::Month))
-        );
-        assert_eq!(
-            unit_abbr.parse_peek(&Partial::new("w")),
-            Ok(("", TimeUnit::Week))
-        );
-        assert_eq!(
-            unit_abbr.parse_peek(&Partial::new("d")),
-            Ok(("", TimeUnit::Day))
-        );
-        assert_eq!(
-            unit_abbr.parse_peek(&Partial::new("h")),
-            Ok(("", TimeUnit::Hour))
-        );
-        assert_eq!(
-            unit_abbr.parse_peek(&Partial::new("m")),
-            Ok(("", TimeUnit::Minute))
-        );
-        assert_eq!(
-            unit_abbr.parse_peek(&Partial::new("s")),
-            Ok(("", TimeUnit::Second))
-        );
-        assert_eq!(
-            unit_abbr.parse_peek(&Partial::new("ms")),
-            Ok(("", TimeUnit::MilliSecond))
-        );
-        assert_eq!(
-            unit_abbr.parse_peek(&Partial::new("µs")),
-            Ok(("", TimeUnit::MicroSecond))
-        );
-        assert_eq!(
-            unit_abbr.parse_peek(&Partial::new("ns")),
-            Ok(("", TimeUnit::NanoSecond))
-        );
-    }
-
-    #[test]
-    fn test_time_unit() {
-        let (input, format) = unit_abbr.parse_peek("m123").unwrap();
-        assert_eq!(input, "123");
-        assert_eq!(format, TimeUnit::Minute);
-    }
+    use crate::{catch_err, CondUnit, TimeUnit};
 
     #[test]
     fn test_parse_expr_time() {
@@ -191,7 +102,7 @@ mod tests {
 
     #[test]
     fn test_cond_unit() {
-        let (input, format) = cond_unit.parse_peek("*123").unwrap();
+        let (input, format) = cond_unit1.parse_peek("*123").unwrap();
         assert_eq!(input, "123");
         assert_eq!(format, CondUnit::Star);
     }
@@ -255,38 +166,28 @@ mod tests {
         assert_eq!(duration, Duration::new(144, 0))
     }
 
-    //TODO lost cause, need fix
     #[test]
     fn test_duration_err() {
         assert_eq!(
-            parse("0m+3-5").err().unwrap(),
+            catch_err!(parse("0m+3-5")),
             r#"
 0m+3-5
     ^
-partial_input:`-5`,error Eof"#
-                //             r#"
-                // 0m+3-5
-                //     ^
-                // partial_input:`-5`,error Fail,`expect one of:['+','*'], but find:-`"#
+partial_input:-5, expect one of :["y", "mon", "w", "d", "h", "m", "s", "ms", "µs", "us", "ns"] or their longer forms, but find:-5"#
                 .trim()
         );
 
-        let err = format!("{}", parse("0mxyz").err().unwrap());
-        assert_eq!(err, r#"
+        assert_eq!(catch_err!(parse("0mxyz")), r#"
 0mxyz
  ^
-partial_input:`mxyz`,error Fail,`expect one of :["y", "mon", "w", "d", "h", "m", "s", "ms", "µs", "us", "ns"] or their longer forms, but find:mxyz`"#.trim());
+partial_input:mxyz, expect one of :["y", "mon", "w", "d", "h", "m", "s", "ms", "µs", "us", "ns"] or their longer forms, but find:mxyz"#.trim());
 
-        let err = format!("{}", parse("3ms-2ms").err().unwrap());
         assert_eq!(
-            err,
-            "3ms-2ms
+            catch_err!(parse("3ms-2ms")),
+            r#"
+3ms-2ms
    ^
-partial_input:`-2ms`,error Eof"
-                //             r#"
-                // 3ms-2ms
-                //    ^
-                // partial_input:`-2ms`,error Fail,`expect one of:['+','*'], but find:-`"#
+partial_input:-2ms, expect one of:['+','*'], but find:-2ms"#
                 .trim()
         );
     }
@@ -337,22 +238,18 @@ partial_input:`-2ms`,error Eof"
 
         let duration = parse("1d2h3min4s").unwrap();
         assert_eq!(duration, Duration::new(93784, 0));
-
-        // 1d2h3min4s
-        // 3m + 31
     }
 
     #[test]
     fn test_overflow_plus() {
-        let result = parse("10000000000000000y+60");
         assert_eq!(
-            result,
-            Err(r#"
+            catch_err!(parse("10000000000000000y+60")),
+            r#"
 10000000000000000y+60
 ^
-partial_input:`10000000000000000y+60`,error Verify,overflow error"#
+partial_input:10000000000000000y+60, overflow error"#
                 .trim()
-                .to_string())
+                .to_string()
         );
     }
 
